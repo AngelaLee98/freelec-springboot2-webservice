841f59909affa5857b5b863c1d46a186c1c3f263
angelalee <janhyang0309@gmail.com>
2021-08-02 23:03:58 +0900

    2021.08.02 공부분

---
+++ src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.dto;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트(){
+        String name="test";
+        int amount = 1000;
+
+        HelloResponseDto dto= new HelloResponseDto(name,amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
+
+//assertThat : 테스트 검증 라이브러리의 검증 메소드
+//isEqualTo : 동등 비교 메소드 , assertThat에 있는 값과 isEqualTo의 값을 비교해서 같을 때만 성공
